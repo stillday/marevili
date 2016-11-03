6c6ecc90e752549320393d5c2b99d30227f049d1
Jan-Dennis Drenkhahn <Jan-Dennis Drenkhahn>
2016-11-03 22:37:03 +0100

    google api aktiviert, daten aufgeteilt

--- main.py
+++ main.py
@@ -1,54 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-import jinja2
+
 import webapp2
 import json
-#import simplejson, urllib
+import urllib
 from google.appengine.api import urlfetch
-from google.appengine.ext import ndb
 from google.appengine.api import users
 
-
-template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
-
-
-class BaseHandler(webapp2.RequestHandler):
-
-    def write(self, *a, **kw):
-        return self.response.out.write(*a, **kw)
-
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
-
-    def render(self, template, **kw):
-        return self.write(self.render_str(template, **kw))
-
-    def render_template(self, view_filename, params=None):
-        if not params:
-            params = {}
-        template = jinja_env.get_template(view_filename)
-        return self.response.out.write(template.render(params))
-
-################################################################################################################
-# Google Place Api Handler
-################################################################################################################
-GEOCODE_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
-
-def geocode(address, **geo_args):
-    geo_args.update({
-        'address': address
-    })
-
-    url = GEOCODE_BASE_URL + '?' + urllib.urlencode(geo_args)
-    result = simplejson.load(urllib.urlopen(url))
-
-    print simplejson.dumps([s['formatted_address'] for s in result['results']], indent=2)
-
-if __name__ == '__main__':
-    geocode(address="San+Francisco")
+from base import BaseHandler
+from model import Gastro, Recommendation, Review
 
 ######################################################################################################################
 # Main System with Input and Detail Handler
@@ -99,26 +59,9 @@ class InputHandler(BaseHandler): #reading the input information
         rating = int(self.request.get("rating")) #Self Rating of the Restaurant
         rekitchen = self.request.get("kueche") #what for kitchen gives in the Restaurant
 
-        lokal = Gastro(lokal_user = user.email(), lokal_name = rename, lokal_street = restreet, lokal_plz = replz, lokal_place = replace, lokal_note = reinfo, lokal_time = revisit, lokal_kitchen = rekitchen, lokal_rating = rating, lokal_price = price)
+        lokal = Gastro(user = user.email(), name = rename, street = restreet, plz = replz, place = replace, note = reinfo, time = revisit, kitchen = rekitchen, rating = rating, price = price)
         lokal.put()
-        gastros = Gastro.query().fetch()
-        user = users.get_current_user()
-        logged_in = user is not None
-        params = {"gastros": gastros, "user": user, "logged_in": logged_in}
-        return self.redirect_to("rest-list", params=params)
-
-class Gastro(ndb.Model): #push in die datenbank
-    lokal_user = ndb.StringProperty()
-    lokal_name = ndb.StringProperty()
-    lokal_street = ndb.StringProperty()
-    lokal_plz = ndb.StringProperty()
-    lokal_place = ndb.StringProperty()
-    lokal_note = ndb.StringProperty()
-    lokal_time = ndb.StringProperty()
-    lokal_kitchen = ndb.StringProperty()
-    lokal_price = ndb.IntegerProperty()
-    lokal_rating = ndb.IntegerProperty()
-    created = ndb.DateTimeProperty(auto_now_add=True)
+        return self.redirect_to("rest-list")
 
 ################################################################################################################
 #Restaurant Detail Handler
@@ -126,14 +69,19 @@ class Gastro(ndb.Model): #push in die datenbank
 
 class DetailHandler(BaseHandler):
     def get(self, redetail_id):
-       # url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=seoul+wien&key=AIzaSyBKdIPR1Q6TzIvjJuJzIyvybo6Mg1JLm64"
-
-        #result = urlfetch.fetch(url)
-
-       # restaurant_info = json.loads(result.content)
-
         redetail = Gastro.get_by_id(int(redetail_id))
-        params = {"redetail": redetail}
+
+        q = urllib.urlencode({
+            "query": (redetail.name + " in " + redetail.place).encode(encoding='ascii', errors='ignore'),
+            "key": "AIzaSyBKdIPR1Q6TzIvjJuJzIyvybo6Mg1JLm64"
+        })
+
+        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?" + q
+        result = urlfetch.fetch(url)
+
+        restaurant_info = json.loads(result.content)
+
+        params = {"redetail": redetail, "restaurant_info": restaurant_info}
         return self.render_template("rest_details.html", params=params)
 
 
@@ -161,23 +109,14 @@ class ReviewHandler(BaseHandler):
         new_visit = self.request.get("newvisit")
 
         reviews = Gastro.get_by_id(int(reviews_id))
-        reviews.lokal_note = new_review
-        reviews.lokal_price = new_price
-        reviews.lokal_rating = new_rating
-        reviews.lokal_visit = new_visit
+        reviews.note = new_review
+        reviews.price = new_price
+        reviews.rating = new_rating
+        reviews.visit = new_visit
         reviews.put()
         redetail = Gastro.get_by_id(int(reviews_id))
         params = {"redetail": redetail}
         return self.render_template("rest_details.html", params=params)
-
-
-class Review(ndb.Model):
-    review_user = ndb.StringProperty()
-    review_note = ndb.StringProperty()
-    review_price = ndb.IntegerProperty()
-    review_rating = ndb.IntegerProperty()
-    review_visit = ndb.StringProperty()
-    created = ndb.DateTimeProperty(auto_now_add=True)
 
 
 #######################################################################################################################
@@ -218,23 +157,10 @@ class RecomInputHandler(BaseHandler):
         recprice = int(self.request.get("price")) #Price Ranking
         reckitchen = self.request.get("kueche") #what kind of kitchen
 
-        recom = Recommendation(recom_user = user.email(), recom_name = recname, recom_street = recstreet, recom_plz = recplz, recom_place = recplace, recom_from = recfrom, recom_user_self = recuserself, recom_price = recprice, recom_kitchen = reckitchen)
+        recom = Recommendation(user = user.email(), name = recname, street = recstreet, plz = recplz, place = recplace, by = recfrom, user_self = recuserself, price = recprice, kitchen = reckitchen)
         recom.put()
-        recoms = Recommendation.query().fetch()
-        params = {"recoms": recoms}
-        return self.redirect_to("rec-list", params=params)
+        return self.redirect_to("rec-list")
 
-class Recommendation(ndb.Model):
-    recom_user = ndb.StringProperty()
-    recom_name = ndb.StringProperty()
-    recom_street = ndb.StringProperty()
-    recom_plz = ndb.StringProperty()
-    recom_place = ndb.StringProperty()
-    recom_from = ndb.StringProperty()
-    recom_user_self = ndb.StringProperty()
-    recom_price = ndb.IntegerProperty()
-    recom_kitchen = ndb.StringProperty()
-    created = ndb.DateTimeProperty(auto_now_add=True)
 
 ##################################
 #Recommendation Detail          ##
@@ -272,17 +198,13 @@ class RecomToRest(BaseHandler):
         rating = int(self.request.get("rating"))  # Self Rating of the Restaurant
         rekitchen = self.request.get("kueche")  # what for kitchen gives in the Restaurant
 
-        lokal = Gastro(lokal_user=user.email(), lokal_name=rename, lokal_street=restreet, lokal_plz=replz,
-                       lokal_place=replace, lokal_note=reinfo, lokal_time=revisit, lokal_kitchen=rekitchen,
-                       lokal_rating=rating, lokal_price=price)
+        lokal = Gastro(user=user.email(), name=rename, street=restreet, plz=replz,
+                       place=replace, note=reinfo, time=revisit, kitchen=rekitchen,
+                       rating=rating, price=price)
         lokal.put()
-        gastros = Gastro.query().fetch()
-        user = users.get_current_user()
-        logged_in = user is not None
-        params = {"gastros": gastros, "user": user, "logged_in": logged_in}
         recoms = Recommendation.get_by_id(int(recoms_id))
         recoms.key.delete()
-        return self.redirect_to("rest-list", params=params)
+        return self.redirect_to("rest-list")
 
 
 
@@ -294,7 +216,7 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name="rest-list"),
     webapp2.Route('/input.html', InputHandler),
     webapp2.Route('/recommendation.html', RecommendationHandler, name="rec-list"),
-    webapp2.Route('/recom_input.html', RecomInputHandler),
+    webapp2.Route('/input.html', RecomInputHandler),
     webapp2.Route('/restaurant/<reviews_id:\d+>/review', ReviewHandler),
     webapp2.Route('/restaurant/<redetail_id:\d+>/details', DetailHandler, name="review-list"),
     webapp2.Route('/recommendation/<recoms_id:\d+>/details', RecomDetailHandler),
---
+++ model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from google.appengine.ext import ndb
+
+
+class Review(ndb.Model):
+    user = ndb.StringProperty()
+    note = ndb.StringProperty()
+    price = ndb.IntegerProperty()
+    rating = ndb.IntegerProperty()
+    visit = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class Recommendation(ndb.Model):
+    user = ndb.StringProperty()
+    name = ndb.StringProperty()
+    street = ndb.StringProperty()
+    plz = ndb.StringProperty()
+    place = ndb.StringProperty()
+    by = ndb.StringProperty()
+    user_self = ndb.StringProperty()
+    price = ndb.IntegerProperty()
+    kitchen = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class Gastro(ndb.Model): #push in die datenbank
+    user = ndb.StringProperty()
+    name = ndb.StringProperty()
+    street = ndb.StringProperty()
+    plz = ndb.StringProperty()
+    place = ndb.StringProperty()
+    note = ndb.StringProperty()
+    time = ndb.StringProperty()
+    kitchen = ndb.StringProperty()
+    price = ndb.IntegerProperty()
+    rating = ndb.IntegerProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
